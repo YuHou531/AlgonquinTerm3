e292be871a2529ca4bd6e210ffddd2ec904e793d
Yu Hou <yuhou531@gmail.com>
2016-11-20 22:42:18 -0500

    start Lab04

---
+++ CST8288Lab04/src/mycollections/Queue.java
@@ -0,0 +1,62 @@
+package mycollections;
+
+import java.util.Iterator;
+
+/**
+ * Queue ADT
+ * 
+ * @author    Yu Hou
+ * @version   Nov 20 2016
+ *
+ */
+public interface Queue<E> {
+	
+	/**
+	 * Inserts an element at the rear of the queue
+	 * Adds the specified element to this queue
+	 * 
+	 * @param o new element to be inserted
+	 */
+	public boolean enqueue(E o); 
+	
+	/**
+	 * Retrieves, but does not remove, the head of this queue
+	 * an error occurs if the queue is empty
+	 */
+	public E peek();
+	
+	/**
+	 * Retrieves and removes the head of this queue
+	 * an error occurs if the queue is empty
+	 */
+	public E dequeue();
+	
+	/**
+	 * Returns the number of elements in this collection 
+	 */
+	public int size();
+	
+	/**
+	 * Remove all elements from the queue
+	 */
+	public void clear();
+	
+	/**
+	 * Returns an iterator over the elements in this queue
+	 */
+	public Iterator<E> iterator();
+	
+	/**
+	 * Removes a single instance of the specified element
+	 * from the collection if it is present (optional operation)
+	 * 
+	 * @param o Object to remove
+	 */
+	public boolean remove(Object o);
+	
+	/**
+	 * Returns true if object "o" is present in the queue
+	 * @param o Object to check contains
+	 */
+	public boolean contains(Object o);
+}
